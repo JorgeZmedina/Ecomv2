c6fb36004b34c999863f6748b8b0b08062c98159
JorgeZmedina <114126984+JorgeZmedina@users.noreply.github.com>
2023-04-04 13:15:29 -0400

    fixing products page

--- icoffee/src/pages/Products.js
+++ icoffee/src/pages/Products.js
@@ -1,18 +1,38 @@
 import React from "react";
-import '../styles/products.css';
-import Cards from '../components/cards'
-
-const losProducts = () => {
-    return(
-        <>
-            <h1>Los Productos!!</h1>
-            <div className="main-products">
-            <Cards />
-            <Cards />
-            <Cards />
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import '../styles/products.css'
+const Products = () => {
+  const [Products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/products");
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllProducts();
+  }, []);
+  console.log(Products);
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <div className="products-container">
+        {Products.map((product) => (
+          <div key={product.id} className="product-item">
+            <img src={product.image} alt={product.title} />
+            <div className="product-details">
+              <h2>{product.title}</h2>
+              <p>{product.description}</p>
+              <span className="product-price">${product.price}</span>
             </div>
-        </>
-    )
-}
-
-export default losProducts;
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Products;
