26716d97e79e17afb90c866ec6ec07a25285452f
JorgeZmedina <114126984+JorgeZmedina@users.noreply.github.com>
2023-04-05 01:09:26 -0400

    fixes

--- icoffee/src/pages/Products.js
+++ icoffee/src/pages/Products.js
@@ -1,48 +1,54 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import axios from "axios";
-import '../styles/products.css'
-import Search from "../components/Search";
-function thankyou () {
-    alert("Added To Cart :)")}
-const Products = () => {
-  const [Products, setProducts] = useState([]);
-  useEffect(() => {
-    const fetchAllProducts = async () => {
-      try {
-        const res = await axios.get("http://localhost:8800/products");
-        setProducts(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchAllProducts();
-  }, []);
-  console.log(Products);
+// import React from "react";
+// import { useEffect } from "react";
+// import { useState } from "react";
+// import axios from "axios";
+// import '../styles/products.css'
+// import Search from "../components/Search";
+// function thankyou () {
+//     alert("Added To Cart :)")};
+// const Products = () => {
+//   const [Products, setProducts] = useState([]);
+//   useEffect(() => {
+//     const fetchAllProducts = async () => {
+//       try {
+//         const res = await axios.get("http://localhost:8800/products");
+//         setProducts(res.data);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+//     fetchAllProducts();
+//   }, []);
+//   console.log(Products);
 
 
-  return (
-    <div>
-      <h1>Products Page</h1>
-      <Search />
-      <div className="products-container">
-        {Products.map((product) => (
-          <div key={product.id} className="product-item">
-            <img src={product.image} alt={product.title} />
-            <div className="product-details">
-              <h2>{product.title}</h2>
-              <p>{product.description}</p>
-              <span className="product-price">${product.price}</span>
-              <button className="buttonC buttonD" onClick={thankyou} type="submit" value="submit" id="Submit">Add To Cart</button>
-            </div>
-            
-          </div>
-        ))}
-      </div>
+//   return (
+//     <div>
+//       <h1>Products Page</h1>
+//       <Search />
+//       <div className="products-container">
+//         {Products.map((product) => (
+//           <div key={product.id} className="product-item">
+//             <img src={product.image} alt={product.title} />
+//             <div className="product-details">
+//               <h2>{product.title}</h2>
+//               <p>{product.description}</p>
+//               <span className="product-price">${product.price}</span>
+//               <button className="buttonC buttonD" onClick={thankyou} type="submit" value="submit" id="Submit">Add To Cart</button>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
 
       
-    </div>
-  );
+//     </div>
+//   );
+// };
+// export default Products;
+import React from "react";
+const Products = () => {
+    return (
+        <h1>Products Page</h1>
+    );
 };
 export default Products;
